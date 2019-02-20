64dc6ad49d3d81d16859964a2bb5787bc755f7c9
joshua-hurn <joshua.hurn@hotmail.com>
2019-02-19 20:55:01 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 4);
+            if (randomNumber == 3)
+            {
+                Console.WriteLine("Number was 3 this time!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Number was not 3 :(");
+                Console.ReadLine();
+            }
+        }
+    }
+}
